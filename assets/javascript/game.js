dd8b67b94052f2c79a00cde67c52cd6641641850
Aaron L <aaronlng05@gmail.com>
2019-02-04 19:57:33 -0500

    pyschic-game basic structure

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+//defining array for computer choices
+var computerChoices = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
+
+//variables to hold values of wins, losses, attempts, and guesses so far.
+var wins = 0;
+var losses = 0;
+var gleft = 9;
+var gsofar = ;
+
+//variables for the reference in the body
+var winsText = document.getElementById("wins-text")
+var lossesText = document.getElementById("losses-text")
+var gleftText = document.getElementById("gleft-text")
+var gsofarText = document.getElementById("gsofar-text")
