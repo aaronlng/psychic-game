54588897c361876c6613856823e6cd841bdd6fd4
Aaron L <aaronlng05@gmail.com>
2019-02-09 09:09:31 -0500

    psychic-game-V2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,10 +5,41 @@ var computerChoices = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s
 var wins = 0;
 var losses = 0;
 var gleft = 9;
-var gsofar = ;
 
 //variables for the reference in the body
-var winsText = document.getElementById("wins-text")
-var lossesText = document.getElementById("losses-text")
-var gleftText = document.getElementById("gleft-text")
-var gsofarText = document.getElementById("gsofar-text")
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var gleftText = document.getElementById("gleft-text");
+var gsofarText = document.getElementById("gsofar-text");
+
+//computer guess onload
+// document.body.onload = function () {
+    
+     //display the content
+     winsText.textContent = "Wins: " + wins;
+     lossesText.textContent = "Losses: " + losses;
+     gleftText.textContent = "Guesses Left: " + gleft;
+     gsofarText.textContent = "Your Guesses so far: ";
+// };
+
+//defining computer selection
+var computerSelection = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+document.onkeyup = function(event) {
+
+    //determining user selection
+    var userSelect = event.key;
+
+    //determining if user guessed correct key
+    if (userSelect === computerSelection) {
+        wins++;
+    }
+    else {
+        losses++;
+        gleft--;
+    }
+};
+
+
+
+
